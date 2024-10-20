884375158d88237e0e5c5a876abc481ebf81755d
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-10-20 18:48:08 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Figtree:wght@700&family=Montserrat:wght@400;500&family=Poppins:wght@400&family=Exo+2:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={` antialiased`}>
         {children}
       </body>
     </html>
